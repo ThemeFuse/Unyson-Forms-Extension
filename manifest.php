da8111f87f939f43fa6a1bd51322484d16423bd6
moldcraft <moldcraft@email.com>
2014-12-30 16:09:17 +0200

    Added the `github_update` parameter in manifest

--- manifest.php
+++ manifest.php
@@ -4,10 +4,10 @@ $manifest = array();
 
 $manifest['name'] = __('Forms', 'fw');
 $manifest['description'] = __('This extension adds the possibility to create a contact form. Use the drag & drop form builder to create any contact form you\'ll ever want or need.', 'fw');
-$manifest['version'] = '1.0.1';
+$manifest['version'] = '1.0.2';
 $manifest['standalone'] = false;
 $manifest['display'] = true;
-
+$manifest['github_update'] = 'ThemeFuse/Unyson-Forms-Extension';
 $manifest['requirements'] = array(
 	'extensions' => array(
 		'page-builder' => array(), // this is the only way to display the built form
