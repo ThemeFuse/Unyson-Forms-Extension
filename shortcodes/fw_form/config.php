ed4fdf1cace9e779659ad6e19d8059d51f84bedf
Nicolai Slovineanu <rockalife@gmail.com>
2015-01-20 11:43:37 +0200

    Update config.php

--- shortcodes/fw_form/config.php
+++ shortcodes/fw_form/config.php
@@ -4,6 +4,6 @@ $cfg = array();
 
 $cfg['page_builder'] = array(
 	'title' => __('Form', 'fw'),
-	'description' => __('Form', 'fw'),
+	'description' => __('Add a Form', 'fw'),
 	'tab' => __('Content Elements', 'fw'),
 );
