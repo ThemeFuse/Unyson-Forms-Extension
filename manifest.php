f5de39be8fd9d56b65d7f11f23303281a3cf2f22
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2015-02-11 18:02:09 +0200

    New version 2.0.2

--- manifest.php
+++ manifest.php
@@ -4,7 +4,7 @@ $manifest = array();
 
 $manifest['name'] = __('Forms', 'fw');
 $manifest['description'] = __('This extension adds the possibility to create a contact form. Use the drag & drop form builder to create any contact form you\'ll ever want or need.', 'fw');
-$manifest['version'] = '2.0.1';
+$manifest['version'] = '2.0.2';
 $manifest['standalone'] = false;
 $manifest['display'] = false;
 $manifest['github_update'] = 'ThemeFuse/Unyson-Forms-Extension';
