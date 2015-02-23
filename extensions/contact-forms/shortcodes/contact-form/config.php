ef1e0342359269f5833a16b385e36ca97305f701
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2015-02-23 11:40:13 +0200

    Added contact form popup size

--- extensions/contact-forms/shortcodes/contact-form/config.php
+++ extensions/contact-forms/shortcodes/contact-form/config.php
@@ -8,5 +8,6 @@ $cfg['page_builder'] = array(
 	'title'       => __( 'Contact form', 'fw' ),
 	'description' => __( 'Build contact forms', 'fw' ),
 	'tab'         => __( 'Content Elements', 'fw' ),
+	'popup_size'  => 'large',
 	'type'        => 'special'
 );
