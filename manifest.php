70c0726eb797e5e385e94370d7f0c7b158ff3bf5
moldcraft <moldcraft@email.com>
2015-06-17 16:16:24 +0300

    Version 2.0.9

--- manifest.php
+++ manifest.php
@@ -3,8 +3,12 @@
 $manifest = array();
 
 $manifest['name'] = __('Forms', 'fw');
-$manifest['description'] = __('This extension adds the possibility to create a contact form. Use the drag & drop form builder to create any contact form you\'ll ever want or need.', 'fw');
-$manifest['version'] = '2.0.8';
+$manifest['description'] = __(
+	'This extension adds the possibility to create a contact form.'
+	.' Use the drag & drop form builder to create any contact form you\'ll ever want or need.',
+	'fw'
+);
+$manifest['version'] = '2.0.9';
 $manifest['standalone'] = false;
 $manifest['display'] = false;
 $manifest['github_update'] = 'ThemeFuse/Unyson-Forms-Extension';
