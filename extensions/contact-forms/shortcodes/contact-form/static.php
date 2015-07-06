caa2dd339ba88759af145377bc8e44749f82c8ae
moldcraft <moldcraft@email.com>
2015-07-06 14:21:35 +0300

    fixed https://github.com/ThemeFuse/Unyson/issues/712#issuecomment-118820248

--- extensions/contact-forms/shortcodes/contact-form/static.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
-
-if ( is_admin() ) {
-	wp_enqueue_style( 'fw-short-code-contact-form-style',
-		fw_ext('contact-forms')->get_uri( '/shortcodes/contact-form/static/css/style.css' )
-	);
-}
