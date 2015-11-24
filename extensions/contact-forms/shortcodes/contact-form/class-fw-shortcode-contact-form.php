c6c74e587fa9a0ba396144a50ba89b68a7e6df12
moldcraft <moldcraft@email.com>
2015-11-24 12:04:29 +0200

    fix for https://github.com/ThemeFuse/Unyson/issues/1043

--- extensions/contact-forms/shortcodes/contact-form/class-fw-shortcode-contact-form.php
+++ extensions/contact-forms/shortcodes/contact-form/class-fw-shortcode-contact-form.php
@@ -9,14 +9,9 @@ class FW_Shortcode_Contact_Form extends FW_Shortcode
 	 */
 	public function _init()
 	{
-		if (is_admin()) {
-			$this->load_item_type();
+		if (is_admin() && fw_ext('page-builder')) {
+			require $this->get_declared_path('/includes/item/class-page-builder-contact-form-item.php');
 		}
-	}
-
-	private function load_item_type()
-	{
-		require $this->get_declared_path('/includes/item/class-page-builder-contact-form-item.php');
 	}
 
 	protected function _render($atts, $content = null, $tag = '')
