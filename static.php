b8990516eba0b2087f93d8f794212d11ccffc370
moldcraft <moldcraft@email.com>
2016-01-09 18:01:45 +0200

    make form styles independent

--- static.php
+++ static.php
@@ -1,10 +1,12 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 if (!is_admin()) {
+	wp_enqueue_style('fw-ext-builder-frontend-grid');
+
 	wp_enqueue_style(
 		'fw-ext-forms-default-styles',
 		fw()->extensions->get('forms')->get_declared_URI('/static/css/frontend.css'),
-		array('fw-ext-builder-frontend-grid'),
+		array(),
 		fw()->manifest->get_version()
 	);
 }
