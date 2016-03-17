5293795817fd082bdbaa18056dfaa79c13b373d1
moldcraft <moldcraft@email.com>
2016-03-17 13:02:39 +0200

    fixes https://github.com/ThemeFuse/Unyson-Forms-Extension/issues/27

--- includes/option-types/form-builder/items/radio/view.php
+++ includes/option-types/form-builder/items/radio/view.php
@@ -6,23 +6,51 @@
  */
 
 $options = $item['options'];
+
+switch ($options['layout']) {
+	case 'one-column': $columns = 1; break;
+	case 'two-columns': $columns = 2; break;
+	case 'three-columns': $columns = 3; break;
+	default: $columns = 0;
+}
 ?>
 <?php if (empty($choices)): ?>
 	<!-- radio not displayed: no choices -->
 <?php else: ?>
 	<div class="<?php echo esc_attr(fw_ext_builder_get_item_width('form-builder', $item['width'] .'/frontend_class')) ?>">
 		<div class="field-radio input-styled">
-			<label><?php echo fw_htmlspecialchars($item['options']['label']) ?>
+			<label><?php echo fw_htmlspecialchars($options['label']) ?>
 				<?php if ($options['required']): ?><sup>*</sup><?php endif; ?>
 			</label>
-			<div class="custom-radio">
-				<?php foreach ($choices as $choice): ?>
-					<?php $choice['id'] = 'rand-'. fw_unique_increment(); ?>
-					<div class="options">
-						<input <?php echo fw_attr_to_html($choice) ?> />
-						<label for="<?php echo esc_attr($choice['id']) ?>"><?php echo $choice['value'] ?></label>
+			<div class="custom-radio field-columns-<?php echo esc_attr($columns); ?>">
+				<?php if ($columns > 1): ?>
+					<?php
+					$choices_count = count($choices);
+					$choices_per_column = abs($choices_count / $columns)
+					                      + (($choices_count % $columns > $columns / 2) ? 1 : 0);
+					$counter = 0;
+					?>
+					<div class="field-column">
+					<?php while ($choice = array_shift($choices)): ?>
+						<?php $choice['id'] = 'rand-'. fw_unique_increment(); ?>
+						<div class="options">
+							<input <?php echo fw_attr_to_html($choice) ?> />
+							<label for="<?php echo esc_attr($choice['id']) ?>"><?php echo $choice['value'] ?></label>
+						</div>
+						<?php if (!(++$counter % $choices_per_column)): ?>
+							</div><div class="field-column">
+						<?php endif; ?>
+					<?php endwhile; ?>
 					</div>
-				<?php endforeach; ?>
+				<?php else: ?>
+					<?php foreach ($choices as $choice):
+						$choice['id'] = 'rand-'. fw_unique_increment(); ?>
+						<div class="options">
+							<input <?php echo fw_attr_to_html($choice) ?> />
+							<label for="<?php echo esc_attr($choice['id']) ?>"><?php echo $choice['value'] ?></label>
+						</div>
+					<?php endforeach; ?>
+				<?php endif; ?>
 			</div>
 			<?php if ($options['info']): ?>
 				<p><em><?php echo $options['info'] ?></em></p>
