8dc0216cf7425d617bc34696786bb33f9e74fd9f
moldcraft <moldcraft@email.com>
2016-05-24 14:43:42 +0300

    fixed hardcode https://github.com/ThemeFuse/Unyson/issues/1576

--- includes/option-types/form-builder/items/form-header-title/view.php
+++ includes/option-types/form-builder/items/form-header-title/view.php
@@ -10,7 +10,7 @@ if ( empty( $title ) ) {
 	return;
 }
 ?>
-<div class="fw-col-xs-12 form-builder-item">
+<div class="<?php echo fw_ext_builder_get_item_width('form-builder', '1_1/frontend_class') ?> form-builder-item">
 	<div class="header title">
 		<h2><?php echo $title; ?></h2>
 		<?php if ( ! empty( $subtitle ) ) : ?>
